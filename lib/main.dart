274d754e2c0252550975823e9cf3a1965b55ed28
elesawy <elesawy928@gmail.com>
2023-09-20 20:07:51 +0300

    create artical model and create news services

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/services/news_services.dart';
 
 import 'views/home_view.dart';
 
 void main(List<String> args) {
+  NewsServices().getNews();
   return runApp(const NewsApp());
 }
 
